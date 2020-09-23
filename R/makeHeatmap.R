2d3e664e397dbbd363f5b1b334b4651b979c1177
RaviDandekar <ravi.dandekar@gmail.com>
2020-09-22 17:03:32 -0700

    modified makeHeatmap so this function can now output a heatmap where the replicates are averaged

--- R/makeHeatmap.R
+++ R/makeHeatmap.R
@@ -13,6 +13,8 @@
 #' @param peptide.id.col column name for peptide IDs in input data frame <default: "peptide_id">
 #' @param min.rpk For heatmaps set 0 values to an inputted minimum RPK value <default: 0>
 #' @param break.list This determines the margins for the resultsing heatmap. the only options are (1,2,3,4) <default: NULL>
+#' @param gene.order Vector of gene names to force the order in which peptides are displayed in the heatmap <default: c()>
+#' @param avg.non.target.columns Average the replicates for all samples that are not in target.samples <default: FALSE>
 #' @return variable containing resulting heatmap.
 #' @examples makeHeatmap(df,patient,target.samples,disease.samples,reference.samples)
 #' @import pheatmap
@@ -22,7 +24,8 @@
 
 makeHeatmap <- function(df,patient,gene.col = "gene",peptide.id.col = "peptide_id",target.samples,
                         disease.samples,reference.samples, normalization.method = "log10",
-                        disease.annot = "COVID-19+",reference.annot = "REFERENCE",min.rpk = 0, break.list = NULL) {
+                        disease.annot = "COVID-19+",reference.annot = "REFERENCE",min.rpk = 0,
+                        break.list = NULL, gene.order = c(),avg.non.target.columns = FALSE) {
   # Set the margins for heatmaps
   par(mar=c(1,1,1,1))
 
@@ -65,6 +68,19 @@ makeHeatmap <- function(df,patient,gene.col = "gene",peptide.id.col = "peptide_i
 
   df <- df[,c(target.samples,disease.samples.rem.order,reference.samples.rem.order)]
 
+  ## Average non-target columns columns ----
+  disease.samples.orig   <- disease.samples
+  reference.samples.orig <- reference.samples
+  if (avg.non.target.columns == TRUE){
+    df <- avgReplicates(df,target.samples,disease.samples,reference.samples)
+    disease.samples   <- collapseReplicateColumnNames(disease.samples)
+    reference.samples <- collapseReplicateColumnNames(reference.samples)
+
+    disease.samples.rem.order   <- collapseReplicateColumnNames(disease.samples.rem.order)
+    reference.samples.rem.order <- collapseReplicateColumnNames(reference.samples.rem.order)
+  }
+
+
   ## Normalize Data
 
   # Min Max normalize
@@ -102,14 +118,45 @@ makeHeatmap <- function(df,patient,gene.col = "gene",peptide.id.col = "peptide_i
   gene_list     <- c()
   peptide_order <- c()
 
+  # This funtions supports type = c("hclust","sumrpk")
+  orderGenes <- function(df_fmt,type = "hclust") {
+    gene_list <- c()
+
+    if (type == "hclust"){
+      heatmap_mtx   <- heatmap.2(as.matrix(df_fmt))
+      all_peptides  <- row.names(df_fmt)
+      peptide_order <- all_peptides[heatmap_mtx$rowInd]
+      gene_list    <- unique(tstrsplit(peptide_order,"_")[[1]])
+    }
+
+    if (type == "sumrpk"){
+      genes <- unique(tstrsplit(row.names(df_fmt),"_")[[1]])
+      sumrpk_vec <- c()
+
+      for (g in genes) {
+        gene_df <- df_fmt[grep(g,row.names(df_fmt)),target.samples]
+        gene_sum_rpk <- sum(rowSums(gene_df))
+        sumrpk_vec <- c(sumrpk_vec,gene_sum_rpk)
+      }
+      names(sumrpk_vec) <- genes
+      sumrpk_vec_sort   <- sort(sumrpk_vec,decreasing = T)
+      gene_list         <- names(sumrpk_vec_sort)
+    }
+
+    return(gene_list)
+  }
+
+
   if(nrow(df_fmt) == 1){
     peptide_order <- row.names(df_fmt)
     gene_list     <- tstrsplit(peptide_order,"_")[[1]]
   } else{
-    heatmap_mtx   <- heatmap.2(as.matrix(df_fmt))
-    all_peptides  <- row.names(df_fmt)
-    peptide_order <- all_peptides[heatmap_mtx$rowInd]
-    gene_list    <- unique(tstrsplit(peptide_order,"_")[[1]])
+    gene_list <- orderGenes(df_fmt,type = "sumrpk")
+
+    # If a gene order is provided, use that instead
+    if (length(gene.order)> 0){
+      gene_list <- gene.order[gene.order %in% gene_list]
+    }
   }
 
   df_fmt[,gene.col] <- tstrsplit(row.names(df_fmt),"_")[[1]]
@@ -136,17 +183,23 @@ makeHeatmap <- function(df,patient,gene.col = "gene",peptide.id.col = "peptide_i
   df_fmt <- df_fmt[gene_list_ordered,]
 
 
-  ## Annotation column Samples
+  ## Annotation column Samples ----
   sample_vec <- names(as.data.frame(df_fmt))
   sample_vec <- sample_vec[! sample_vec %in% c(gene.col,peptide.id.col)]
   diagnosis  <- sample_vec
+
+  # Target Samples
+  diagnosis[diagnosis %in% disease.samples.orig]    <- disease.annot
+  diagnosis[diagnosis %in% reference.samples.orig]  <- reference.annot
+
+  # Remaining samples
   diagnosis[diagnosis %in% disease.samples]    <- disease.annot
   diagnosis[diagnosis %in% reference.samples]  <- reference.annot
 
   diagnosis_annot <- as.data.frame(diagnosis)
   row.names(diagnosis_annot) <- sample_vec
 
-  ## Annotation column Genes
+  ## Annotation column Genes ----
   peptide_vec <- row.names(as.data.frame(df_fmt))
   genes_vec   <- tstrsplit(peptide_vec,"_")[[1]]
   genes_annot <- as.data.frame(genes_vec)
@@ -159,8 +212,18 @@ makeHeatmap <- function(df,patient,gene.col = "gene",peptide.id.col = "peptide_i
   break_list3 <- seq(-3,3,0.06)  # -3 to 3
   break_list4 <- seq(-4,4,0.08)  # -4 to 4
 
+
+  # break list -1 to 4
+  break_1 <- seq(-1,1,0.04)  # first 51 breaks
+  break_2 <- seq(1,4,0.06)  # last 50 breaks
+  break_2 <- break_2[3:length(break_2)]
+  break_list1_4 <- c(break_1,break_2)
+
+  #break_list1_4 <- break_list1[break_list1 < 0]
+  #break_list1_4 <- c(break_list1_4,break_list4[break_list4 >= 0])
+
   break_list = NULL
-  if (normalization.method == "log10")   {break_list = break_list2}
+  if (normalization.method == "log10")   {break_list = break_list1_4}
   if (normalization.method == "minmax")  {break_list = break_list1}
   if (normalization.method == "meannorm"){break_list = break_list1}
 
@@ -179,7 +242,7 @@ makeHeatmap <- function(df,patient,gene.col = "gene",peptide.id.col = "peptide_i
   # Column annotation bars colors
   colcolor        <- c("purple", "darkgreen")
   names(colcolor) <- c(disease.annot, reference.annot)
-  anno_colors <- list(diagnosis = colcolor)
+
 
   # Row annotation gene block colors
   all_genes <- as.character(unique(genes_annot$genes_vec))
@@ -194,12 +257,79 @@ makeHeatmap <- function(df,patient,gene.col = "gene",peptide.id.col = "peptide_i
     }
     rowcolor <- c(rowcolor,temp_col)
   }
+  names(rowcolor) <- all_genes
+
+  # Create annotation color list
+  anno_colors <- list(diagnosis = colcolor,genes_vec = rowcolor)
 
   p <- pheatmap(df_fmt,cluster_cols = F, cluster_rows = F,
                 color = color_pal,breaks = break_list,fontsize_row = 4.5,
                 annotation_col = diagnosis_annot, annotation_row = genes_annot,
                 annotation_legend = F,annotation_names_col = F, annotation_names_row = F,annotation_colors = anno_colors,
-                gaps_col = c(length(target.samples)),cellheight = 5)
+                gaps_col = c(length(target.samples)),cellheight = 5, border_color = NA)
 
   return(p)
 }
+
+
+
+#' @title Average Replicates in Peptide data frame
+#' @description takes input of phage results used for Heatmaps and averages the replicates on non=target columns
+#' @param df phage peptide count matrix
+#' @param target.samples vector of target samples of interest from one patient (defined by 'patient' input)
+#' @param disease.samples vector of all disease samples in input data frame
+#' @param reference.samples vector of all reference samples (healthy and bead controls) in input data frame
+#' @param chop.length number characters to remove from the end of each sample name to remove the replicate IDs <default: 2>
+#' @return data frame with non-target columns averaged
+#' @examples avgReplicates(df,target.samples,disease.samples,reference.samples)
+#' @import data.table
+#' @export
+avgReplicates <- function(df,target.samples,disease.samples,reference.samples,chop.length=2) {
+  df_avg <- df[,target.samples]
+
+  # Format new sample names
+  other.samples     <- names(df)[! names(df) %in% target.samples]
+  other.samples.fmt <- collapseReplicateColumnNames(other.samples,chop.length)
+
+  for (sample in other.samples.fmt) {
+    sample_reps       <- names(df)[grep(sample,names(df))]
+    df_sub            <- as.data.frame(df[,sample_reps])
+    names(df_sub)     <- sample_reps
+    row.names(df_sub) <- row.names(df)
+    if (ncol(df_sub) > 1){
+      df_sub[,sample] <- rowMeans(df_sub)
+      df_avg <- cbind(df_avg,df_sub[,sample])
+    } else{
+      df_avg <- cbind(df_avg,df_sub)
+    }
+  }
+
+  names(df_avg) <- c(target.samples,other.samples.fmt)
+
+  return(df_avg)
+
+}
+
+
+#' @title Format Column names to remove replicate IDs
+#' @description take an input vector of sample names and collapse all replicates
+#' @param col.name.vec input column names, sample names only
+#' @param chop.length number of characters to chop from the end of each sample name <default: 2>
+#' @return vector of unique collapsed sample names
+#' @examples collapseReplicateColumnNames(col.name.vec,chop.length = 2)
+#' @import data.table
+collapseReplicateColumnNames <- function(col.name.vec,chop.length = 2) {
+  for (i in 1:length(col.name.vec)) {
+    sample               <- col.name.vec[i]
+    if (grepl("Bead_",sample)){
+      col.name.vec[i] <- tstrsplit(sample,"_")[[1]]
+    } else{
+      col.name.vec[i] <- substr(sample,1,nchar(sample)-chop.length)
+    }
+
+  }
+  col.name.vec <- unique(col.name.vec)
+
+  return(col.name.vec)
+}
+
