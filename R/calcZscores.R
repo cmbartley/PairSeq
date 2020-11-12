6d8cd36b83b1a01031e96369161458ae230058d2
RaviDandekar <ravi.dandekar@gmail.com>
2020-11-12 14:30:57 -0800

    finished function for Z-score calculations

--- R/calcZscores.R
+++ R/calcZscores.R
@@ -1,10 +1,10 @@
 #' @title Creates Z-score Columns
 #' @description takes input of phage results and calculates Z-scores for every target column based on the input
 #' @param df phage peptide count matrix
-#' @param target.columns list of target samples to analyze
-#' @param method method to use for calculating Z-scores ("median","mean") <default: "median">
+#' @param target.samples vector of target samples to analyze
+#' @param reference.samples vector of reference control samples used for calculating Std. Dev. and average expression
+#' @param method method to use for calculating Z-scores ("median","mean") <default: "mean">
 #' @param MIN minimum count value for the inputted data frame <default: 1>
-#' @param onlyZscores option to format the output and return full data frame or only the Z-score columns <default: FALSE>
 #' @return returns phage results with FC columns for each target sample.
 #' @examples calcZscores(df, target_columns)
 #' @export
@@ -12,43 +12,31 @@
 ## The is code takes a dataframe and calculates the fold change for each column relative to the mean of the control samples
 ## parameters will need to be adjusted  for other dataframes
 
-calcZscores <- function(df, target.samples, method = "median", MIN = 1,peptide.id.col = "peptide_id",onlyZscores = FALSE){
-  peptide_df <- df[,c(target.samples, peptide.id.col)]
+calcZscores <- function(df, target.samples, reference.samples, method = "mean", MIN = 1){
+  peptide_df <- df[,unique(c(target.samples,reference.samples))]
 
+  # calc Std dev per row
+  ref_df <- df[,unique(c(reference.samples))]
+  ref_df <- transform(ref_df, SD=apply(ref_df,1, sd, na.rm = F))
+  peptide_df$SD <- ref_df$SD
+
+  # Calc mean or median
+  ref_df$SD <- NULL
+  ref_df <- transform(ref_df, AVG=apply(ref_df,1, method, na.rm = F))
+  peptide_df$AVG <- ref_df$AVG
+
+  zscore_df  <- data.frame(matrix(nrow = nrow(peptide_df), ncol = 0))
 
   for (sample in target.samples) {
-    sample_df <- as.data.frame(peptide_df[,c(sample,peptide.id.col)])
+    sample_vec <- peptide_df[,sample]
+    zscore_vec <- sample_vec - peptide_df$AVG
+    zscore_vec <- zscore_vec / peptide_df$SD
+    zscore_vec[is.na(zscore_vec)] <- 0
 
-    # Filter based on MIN
-    sample_df_sub <- sample_df[sample_df[,sample] > MIN,]
-
-    # Z-score calculation
-    m <- 0
-    if (method == "mean")   {m <- mean(sample_df_sub[,sample])}
-    if (method == "median") {m <- median(sample_df_sub[,sample])}
-    sd(sample_df_sub[,sample])
-
-
-
-
-
+    zscore_df <- cbind(zscore_df,as.data.frame(zscore_vec))
   }
 
-  df_out <- cbind(df, FC_df)
-  if (onlyFC == TRUE){
-    df_out <- FC_df
-  }
+  names(zscore_df) <- target.samples
 
-
-  #scatter y-pos over plot
-  bead_df_log <- log10(peptide_df$CoV_NID1567_Plasma_1)
-  bead_df_log[bead_df_log == -Inf] <- 0
-  d <- density(bead_df_log)
-  plot(d, main="Test")
-  polygon(d, col="red", border="blue")
-
-  return(df_out)
+  return(zscore_df)
 }
-
-
-
